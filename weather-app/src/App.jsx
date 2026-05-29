31c070eed372a93d9f7934f965e226e95e584c86
Anchalsingh12385 <anchalkumarissingh@gmail.com>
2026-05-29 15:30:13 +0530

    updated

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,4 +1,5 @@
 import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
 //import './App.css'
 
 function App() {
@@ -10,6 +11,12 @@ function App() {
     <>
     <h1>Weather App</h1>
     <Button variant="contained" onClick={handleClick}>
+      Click Me
+    </Button>
+     <Button variant="contained" onClick={handleClick} color="secondary">
+      Click Me
+    </Button>
+    <Button variant="contained" onClick={handleClick} startIcon={<DeleteIcon />}>
       Click Me
     </Button>
         </>
