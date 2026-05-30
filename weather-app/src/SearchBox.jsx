a5e461ff82919b3523955f97490c6e28e5bf17af
Anchalsingh12385 <anchalkumarissingh@gmail.com>
2026-05-30 18:10:53 +0530

    updated

--- weather-app/src/SearchBox.jsx
+++ weather-app/src/SearchBox.jsx
@@ -4,7 +4,22 @@ import Button from '@mui/material/Button';
 import './SearchBox.css';
 export default function SearchBox() {
 const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = "bd7db3f0f977b83c4d87843f1a23a76d";
 
+let getWeatherInfo = async (city) => {
+ let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+ let jsonREsponse = await response.json();
+ console.log(jsonREsponse);
+ let result = {
+    temp: jsonREsponse.main.temp,
+    temp_min: jsonREsponse.main.temp_min,
+    temp_max: jsonREsponse.main.temp_max,
+    humidity: jsonREsponse.main.humidity,
+    feelsLike: jsonREsponse.main.feels_like,
+    weather: jsonREsponse.weather[0].description,
+ };
+ console.log(result);
+}
     let [city, setCity] = useState("");
     
 
@@ -15,6 +30,7 @@ const API_URL = "https://api.openweathermap.org/data/2.5/weather";
         event.preventDefault();
         console.log(city);
         setCity("");
+         getWeatherInfo (city);
     }
     return(
         <div className="search-box">
