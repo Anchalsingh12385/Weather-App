590c94c43a225d8b404036012e15cee1c706a8fd
Anchalsingh12385 <anchalkumarissingh@gmail.com>
2026-05-30 17:38:37 +0530

    updated

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,24 +1,12 @@
-import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
+import SearchBox from "./SearchBox";
 //import './App.css'
 
 function App() {
- let  handleClick = () => {
-    alert("Button Clicked")
-  }
+ 
 
   return (
     <>
-    <h1>Weather App</h1>
-    <Button variant="contained" onClick={handleClick}>
-      Click Me
-    </Button>
-     <Button variant="contained" onClick={handleClick} color="secondary">
-      Click Me
-    </Button>
-    <Button variant="contained" onClick={handleClick} startIcon={<DeleteIcon />}>
-      Click Me
-    </Button>
+      <SearchBox />
         </>
   )
 }
